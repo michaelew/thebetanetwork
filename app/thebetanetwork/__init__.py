ad06279d509a98783394150e2d0f51555bf94c62
michaelew <michaelew@users.noreply.github.com>
2016-01-02 18:15:35 -0500

    Update __init__.py

--- app/thebetanetwork/__init__.py
+++ app/thebetanetwork/__init__.py
@@ -7,14 +7,14 @@ app.secret_key = 'OSAdfhse89ugasognEWt09)e90guug'
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "notificationsbetanetwork@gmail.com"
-app.config["MAIL_PASSWORD"] = "omgpassword!"
+app.config["MAIL_USERNAME"] = ".@gmail.com"
+app.config["MAIL_PASSWORD"] = "."
 
 
 from routes import mail
 mail.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:omgpassword!@localhost/development'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:.@localhost/development'
 
 from models import db
 db.init_app(app)
